f44500b956af7918aa841506883909cbd95cd456
infectiouscontent <infectiouscontent@icloud.com>
2024-07-12 10:29:53 +0800

    changing approach

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import LandingPage from "components/LandingPage";
-
 export default function App() {
   return (
-    <LandingPage />
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
   );
 }
